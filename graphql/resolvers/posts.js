1314acff03ee27b1fda290ff51332a23e1780c39
nettlesoup <sanettleingham@hotmail.co.uk>
2020-10-23 14:54:15 +0100

    Add comments / likes / subscription examples

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 
 const checkAuth = require('../../util/checkAuth');
@@ -40,14 +40,18 @@ module.exports = {
 
 			const post = await newPost.save();
 
+			context.pubsub.publish('NEW_POST', {
+				newPost: post,
+			});
+
 			return post;
 		},
 		async deletePost(_, { postId }, context) {
-			const user = checkAuth(context);
+			const { username } = checkAuth(context);
 
 			try {
 				const post = await Post.findById(postId);
-				if (user.username === post.username) {
+				if (username === post.username) {
 					await post.delete();
 					return 'Post deleted successfully';
 				} else {
@@ -56,6 +60,32 @@ module.exports = {
 			} catch (err) {
 				throw new Error(err);
 			}
+		},
+		async likePost(_, { postId }, context) {
+			const { username } = checkAuth(context);
+
+			const post = await Post.findById(postId);
+			if (post) {
+				if (post.likes.find((l) => l.username === username)) {
+					// post already liked
+					post.likes = post.likes.filter((l) => l.username !== username);
+				} else {
+					// post not yet liked
+					post.likes.push({
+						username,
+						createdAt: new Date().toISOString(),
+					});
+				}
+				await post.save();
+				return post;
+			} else {
+				throw new UserInputError('Post not found');
+			}
+		},
+	},
+	Subscription: {
+		newPost: {
+			subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST'),
 		},
 	},
 };
